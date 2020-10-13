0c5f998c25c1ee82312240998a701f407cb765d9
Lilly <lzha989@aucklanduni.ac.nz>
2020-10-13 08:01:28 +0000

    removed prior code that was commented out

--- main.c
+++ main.c
@@ -3,46 +3,6 @@
 #include <stdlib.h>
 
 
-// char stringLower(char s) { 
-//   if ((s > 64) && (s < 91)) {
-//     return s;
-//   } else {
-//     return s - 32;
-//   }
-// }
-
-
-// char stringUpper(char s) { 
-//   printf("%c", s);
-//   if ((s > 64) && (s < 91)) {
-//     return s + 32;
-//   } else {
-//     return s;
-//   }
-// }
-
-
-// int main(void) {
-
-//   char inputString[1000];
-
-//   printf("Type in the sentence you want to mock: \n");
-//   scanf("%c", inputString);
-
-//   for (int i = 0; i < sizeof(inputString); i++) { 
-//     if (i % 2 == 0) { 
-//       inputString[i] = stringLower(inputString[i]);
-//     } else { 
-//       inputString[i] = stringUpper(inputString[i]);
-//     }
-//   }
-//   printf("%s", inputString);
-
-//   return 0;
-// }
-
-
-
 int main(void) {
 
   char inputString[1000] = {0};
