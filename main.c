e6d9dc910db8619da6f175ce23729975053eedad
Lilly <lzha989@aucklanduni.ac.nz>
2020-10-13 07:10:58 +0000

    how to take ascii values - may need to test strlwr again

--- main.c
+++ main.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-int main(void) {
-
-  char inputString[1000];
-
-  printf("Type in the sentence you want to mock: \n");
-  scanf("%s", inputString);
-
-  for (int i = 0; i < sizeof(inputString); i++) { 
-    if (i % 2 == 0) { 
-      inputString[1] = stringLower(inputString[i]);
-    } else { 
-      strupr(inputString[i]);
-    }
-  }
-  printf("%s", inputString);
-
-  return 0;
-}
-
+#include <stdlib.h>
 
 
 char stringLower(char s) { 
@@ -29,3 +10,35 @@ char stringLower(char s) {
     return s - 32;
   }
 }
+
+
+char stringUpper(char s) { 
+  printf("%c", s);
+  if ((s > 64) && (s < 91)) {
+    return s + 32;
+  } else {
+    return s;
+  }
+}
+
+
+
+int main(void) {
+
+  char inputString[1000];
+
+  printf("Type in the sentence you want to mock: \n");
+  scanf("%c", inputString);
+
+  for (int i = 0; i < sizeof(inputString); i++) { 
+    if (i % 2 == 0) { 
+      inputString[i] = stringLower(inputString[i]);
+    } else { 
+      inputString[i] = stringUpper(inputString[i]);
+    }
+  }
+  printf("%s", inputString);
+
+  return 0;
+}
+
