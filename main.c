66dcd6d8cddd48d68e450d6a6a64962f4ca3615f
Lilly <lzha989@aucklanduni.ac.nz>
2020-10-13 07:33:16 +0000

    accounts for the use of spaces and other characters

--- main.c
+++ main.c
@@ -45,25 +45,35 @@
 
 int main(void) {
 
-  char inputString[5];
+  char inputString[20];
+  int caps = 0;
 
   printf("Type in the sentence you want to mock: \n");
-  scanf("%s", inputString);
+  fgets(inputString, 20, stdin);
+  sscanf(inputString, "%s", inputString);
 
   for (int i = 0; i < sizeof(inputString); i++) { 
-    if (i % 2 == 0) { 
+    if (caps == 0) { 
       if ((inputString[i] > 64) && (inputString[i] < 91)) {
         inputString[i] = inputString[i] + 32;
-      } else {
+        caps = 1;
+      } else if ((inputString[i] > 96) && (inputString[i] < 123)){
         inputString[i] = inputString[i];
+        caps = 1;
+      } else {
+      inputString[i] = inputString[i];
       }
     } else { 
       if ((inputString[i] > 64) && (inputString[i] < 91)) {
         inputString[i] = inputString[i];
-      } else {
+        caps = 0;
+      } else if ((inputString[i] > 96) && (inputString[i] < 123)){
         inputString[i] = inputString[i] - 32;
+        caps = 0;
+      } else {
+      inputString[i] = inputString[i];
       }
-    }
+    } 
   }
 
   for (int i = 0; i < sizeof(inputString); i++) {
